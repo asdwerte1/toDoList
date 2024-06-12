74b9c9f9ceea082b13b1fe147da23a8e2e941cfe
asdwerte1 <samjmarkham@outlook.com>
2024-06-12 15:16:35 +0100

    Potential working php script to update database - test to procede shortly

---
+++ addItem.php
@@ -0,0 +1,37 @@
+<?php
+
+$serverName = "localhost";
+$userName = "sam";
+$password = "Esepe!ed1";
+$dbName = "Tasks";
+
+// Create the connectiona
+$con = new mysqli($serverName, $userName, $password, $dbName);
+if ($con->connect_error) {
+    die("Connection failed :" . $con->connect_error);
+}
+
+$taskName = mysqli_real_escape_string($con, $_POST["title"]);
+$taskDesc = mysqli_real_escape_string($con, $_POST["description"]);
+$priority = $_POST["priority"];
+
+$sql = "INSERT INTO task_list (task_title, task_description, priority, complete)
+        VALUES (?, ?, ?, 0)";
+
+$stmt = $con->prepare($sql);
+
+if (!$stmt) {
+    echo "ERROR PREPARING STATEMENT: " . $con->error;
+} else {
+    $stmt->bind_param("sss", $taskName, $taskDesc, $priority);
+
+    if ($stmt->execute()) {
+        echo "task added successfully";
+    } else {
+        echo "Error adding task: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+
+$con->close();
